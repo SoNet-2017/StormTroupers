9013ee3ab07a16992480121dbe4cf6666b73700c
giancarloavalle <s233534@studenti.polito.it>
2017-04-03 12:58:46 +0200

    Modifica javascript e modifica carattere su portrait

--- projectSite.js
+++ projectSite.js
@@ -6,11 +6,9 @@ function w3_open() {
     document.getElementById("headerBlock").style.marginLeft = "25%";
     document.getElementById("mySidebar").style.width = "25%";
     document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("openNav").style.display = 'none';
 }
 function w3_close() {
     document.getElementById("main").style.marginLeft = "0%";
     document.getElementById("headerBlock").style.marginLeft = "0%";
     document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("openNav").style.display = "inline-block";
 }
