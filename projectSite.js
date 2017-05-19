5d3ea14d43e7b6b24bdc1e41fcf0bb6dedf1553f
mugolini4 <matilde.ugolini@gmail.com>
2017-05-19 12:40:39 +0200

    Inserimento di mockup con slideshow -- da finire

--- projectSite.js
+++ projectSite.js
@@ -1,6 +1,25 @@
 /**
  * Created by giancarloavalle on 02/04/17.
  */
+
+// cose per slidesjow
+var slideIndex = 1;
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length};
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+
 function w3_open() {
     document.getElementById("main").style.marginLeft = "25%";
     document.getElementById("headerBlock").style.marginLeft = "25%";
